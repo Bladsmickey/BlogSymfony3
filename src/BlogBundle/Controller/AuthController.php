699181d4037a14fa00a90111db4aaf0c4bfa7c0a
Esteban Bladimir Ruiz <esbruiz@outlook.es>
2016-09-30 13:56:54 -0400

    Registro de usuarios

--- src/BlogBundle/Controller/AuthController.php
+++ src/BlogBundle/Controller/AuthController.php
@@ -3,9 +3,21 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BlogBundle\Entity\User;
+use BlogBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Encoder\BCryptPasswordEncoder;
 
 class AuthController extends Controller
 {
+	private $session;
+
+	public function __construct(){
+
+		$this->session = new Session();
+
+	} 
+
     public function loginAction()
     {
         $authenticationUtils = $this->get("security.authentication_utils");
@@ -13,6 +25,54 @@ class AuthController extends Controller
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('BlogBundle:Default:login.html.twig', array("error"=> $error, "lastUsername"=> $lastUsername));
+    }
+
+    public function registerAction(Request $request)
+    {
+    	$user = new User();
+    	$form = $this->createForm(UserType::class, $user);
+    	$form->handleRequest($request);
+
+    	if($form->isSubmitted()){
+    		if($form->isValid()){
+    			$em = $this->getDoctrine()->getEntityManager();
+    			$user_repo = $em->getRepository("BlogBundle:User");
+    			$user = $user_repo->findOneBy(array("email"=>$form->get('email')->getData()));
+    	    		if(count($user) == 0){
+    	    							$user = new User();
+    	    		    	    		$user->setName($form->get('name')->getData());
+    	    		    	    		$user->setSurname($form->get('surname')->getData());
+    	    		    	    		$user->setEmail($form->get('email')->getData());
+    	    		
+    	    		    	    		$factory = $this->get("security.encoder_factory");
+    	    		    	    		$encoder = $factory->getEncoder($user);
+    	    		    	    		$password = $encoder->encodePassword($form->get('password')->getData(), $user->getSalt());
+    	    		    	    		$user->setPassword($password);
+    	    		    	    		$user->setRole("ROLE_USER");
+    	    		    	    		$user->setImage(null);
+    	    		    	    		$em = $this->getDoctrine()->getEntityManager();
+    	    		    	    		$em->persist($user);
+    	    		    	    		$flush = $em->flush();
+    	    		    	    		if($flush != null){
+    	    		    	    		$status = "Usuario no registrado";
+    	    		    	    		}else{
+    	    		    	    		$status = "Usuario Registrado";
+    	    		    	    		}
+    	    		    	    	}else{
+    	    		    	    	$status = "El usuario ya se registro";	
+    	    		    	    	}
+    	    		
+    	    	}else{
+    	    		$status = "Formulario Invalido";
+    	    	}
+$this->session->getFlashBag()->add("session", $status);
+    	    }
+
+    	
+
+        return $this->render('BlogBundle:Default:register.html.twig', array(
+        	"form"=>$form->createView()
+        	));
     }
 }
 
---
+++ src/BlogBundle/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array("label" => "Nombres","required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('surname', TextType::class, array("label" => "Apellidos", "required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('email', EmailType::class, array("label" => "Correo","required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('password', PasswordType::class, array("label" => "Contraseña","required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('Registrar', SubmitType::class, array("attr" => array("class" => "btn btn-success col-md-3",)))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlogBundle\Entity\User'
+        ));
+    }
+}
