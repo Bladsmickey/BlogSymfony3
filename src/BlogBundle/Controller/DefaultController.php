031c29f6e6e58f6cf379bef757d282f9a673fa1d
Esteban Bladimir Ruiz <esbruiz@outlook.es>
2016-09-30 15:11:52 -0400

    Logica de tags

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -5,11 +5,64 @@ namespace BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use BlogBundle\Entity\Tag;
+use BlogBundle\Form\TagType;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
+		private $session;
+
+	public function __construct(){
+
+		$this->session = new Session();
+
+	} 
     public function indexAction(Request $request)
     {
       return $this->render('BlogBundle:Default:layout.html.twig', array());
+    }
+
+
+
+    public function addTagAction(Request $request)
+    {
+    	$tag = new Tag();
+    	$form = $this->createForm(TagType::class, $tag);
+    	$form->handleRequest($request);
+    	if($form->isSubmitted()){
+    		if($form->isValid()){
+    			$em = $this->getDoctrine()->getEntityManager();
+    			$tag_repo = $em->getRepository("BlogBundle:Tag");
+    			$tag = $tag_repo->findOneBy(array("name"=>$form->get('name')->getData()));
+    	    		if(count($tag) == 0){
+    	    			$tag = new Tag();
+    	    		    $tag->setName($form->get('name')->getData());
+    	    		    $tag->setDescription($form->get('description')->getData());
+    	    		    $em = $this->getDoctrine()->getEntityManager();
+    	    		    $em->persist($tag);
+    	    		    $flush = $em->flush();
+    	    		    	if($flush != null){
+    	    		    	    $status = "Tag no agregada";
+    	    		    	 }else{
+    	    		    	    $status = "Tag agregada";
+    	    		    	    	}
+    	    		 }else{
+    	    		    $status = "Tag existente";	
+    	    		    }
+    	     }else{
+    	    	$status = "Formulario Invalido";
+    	    }
+			$this->session->getFlashBag()->add("session", $status);
+    	    }
+
+   		$em = $this->getDoctrine()->getEntityManager();
+    	$tags_repo = $em->getRepository("BlogBundle:Tag");
+    	$tags = $tags_repo->findAll();
+
+        return $this->render('BlogBundle:BlogData:addTag.html.twig', array(
+        	"tags"=> $tags,
+        	"form"=>$form->createView()
+        	));
     }
 }
---
+++ src/BlogBundle/Form/TagType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class TagType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array("label" => "Nombre","required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('description', TextAreaType::class, array("label" => "Descripcion","required"=>"required", "attr" => array("class" => "form-control",)))
+            ->add('submit' , SubmitType::class, array("attr" => array("class" => "btn btn-success",)))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlogBundle\Entity\Tag'
+        ));
+    }
+}
